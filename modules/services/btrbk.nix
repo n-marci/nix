9cfc0737b3651a46b2a0fbbfe6209cf2fde7efed
n-marci <neugebauer.marcel@web.de>
2024-05-31 16:01:23 +0200

    started changing btrbk module to get rid of permission issue - not finished

--- modules/services/btrbk.nix
+++ modules/services/btrbk.nix
@@ -60,5 +60,22 @@ with lib; {
         };
       };
     };
+
+    security.sudo = {
+      enable = true;
+      extraRules = [{
+        command = "${pkgs.coreutils-full}/bin/test";
+        options = [ "NOPASSWD" ];
+      }
+      {
+        command = "${pkgs.coreutils-full}/bin/readlink";
+        options = [ "NOPASSWD" ];
+      }
+      {
+        command = "${pkgs.btrfs-progs}/bin/btrfs";
+        options = [ "NOPASSWD" ];
+      }];
+      users = [ "btrbk" ];
+    }
   };
 }

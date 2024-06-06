11bd8f5cff0c1e95f8d1f740c1bc0803e77d27c4
Marcel Neugebauer <neugebauer.marcel@web.de>
2024-06-06 13:48:56 +0200

    updated flake and state version for yoga device

--- hosts/yoga/default.nix
+++ hosts/yoga/default.nix
@@ -138,11 +138,11 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   # system.stateVersion = "23.05"; # Did you read the comment?
 
-  system.stateVersion = "23.05"; # Did you read the comment?
+  system.stateVersion = "23.11"; # Did you read the comment?
 
   home-manager.users.${vars.user} = {
     home = {
-      stateVersion = "23.05";
+      stateVersion = "23.11";
     };
 
     programs = {

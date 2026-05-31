5571ab24b50ee8add46db441df7179b222f6605e
n-marci <neugebauer.marcel@web.de>
2026-05-31 03:17:01 +0200

    added nofail option for inspirion/disks.nix for all subvolumes since it was preventing boot

--- hosts/inspirion/disks.nix
+++ hosts/inspirion/disks.nix
@@ -36,105 +36,105 @@ in
                 subvolumes = {
                   "@" = {
                     mountpoint = "/";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@nix" = {
                     mountpoint = "/nix";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@persist" = {
                     mountpoint = "/persist";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
 
                   # /var/lib
                   "@tailscale" = {
                     mountpoint = "/var/lib/tailscale";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@immich" = {
                     mountpoint = "/var/lib/immich";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@nextcloud" = {
                     mountpoint = "/var/lib/nextcloud";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@postgresql" = {
                     mountpoint = "/var/lib/postgresql";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@psql-export" = {
                     mountpoint = "/var/lib/psql-export";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@paperless" = {
                     mountpoint = "/var/lib/paperless";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@samba" = {
                     mountpoint = "/var/lib/samba";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@audiobookshelf" = {
                     mountpoint = "/var/lib/audiobookshelf";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@ocis" = {
                     mountpoint = "/var/lib/ocis";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@opencloud" = {
                     mountpoint = "/var/lib/opencloud";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
 
                   # /var/lib/private
                   "@actual" = {
                     mountpoint = "/var/lib/private/actual";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@AdGuardHome" = {
                     mountpoint = "/var/lib/private/AdGuardHome";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@mealie" = {
                     mountpoint = "/var/lib/private/mealie";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@newt" = {
                     mountpoint = "/var/lib/private/newt";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
 
                   # syncthing
                   "@syncthing-nix" = {
                     mountpoint = "/var/lib/syncthing/nix";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@secrets" = {
                     mountpoint = "/var/lib/syncthing/secrets";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@wallpapers" = {
                     mountpoint = "/var/lib/syncthing/wallpapers";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@obsidian" = {
                     mountpoint = "/var/lib/syncthing/obsidian";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@phone" = {
                     mountpoint = "/var/lib/syncthing/phone";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@signal" = {
                     mountpoint = "/var/lib/syncthing/signal";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                   "@whatsapp" = {
                     mountpoint = "/var/lib/syncthing/whatsapp";
-                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountOptions = [ "compress=zstd" "noatime" "nofail" ];
                   };
                 };
               };
